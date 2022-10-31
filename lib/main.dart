b187640df8ddf32223025dfeac678dfe57cb26fa
Edwin bernal <diomedes1233@gmail.com>
2022-10-31 08:38:33 -0500

    se imagen a el home

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Turyn Viajes',
+      title: 'Tures y Viajes',
       localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
